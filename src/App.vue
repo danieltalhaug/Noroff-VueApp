ffa95c23890337fa3e48baa975e35921cfce709c
Daniel Talhaug <32128980+danieltalhaug@users.noreply.github.com>
2019-03-08 14:37:52 +0100

    Added files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <RecipePuppy/>
+  </div>
+</template>
+
+<script>
+
+import RecipePuppy from './components/RecipeComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    RecipePuppy
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/RecipeComponent.vue
@@ -0,0 +1,44 @@
+<template v-for="recipe in recipes" >
+ <div class="card">
+    <img :src="recipes.results[0].thumbnail">
+    <h2>{{recipes.results[0].title}}</h2>
+    <p>{{recipes.results[0].ingredients}}</p>
+    <a :href="recipes.results[0].href">Check it out</a>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipePuppy',
+  data() {
+    return {
+      recipes: []
+    }
+  },
+  beforeMount: function() {
+    const app = this;
+    const conversionUrl = 'https://cors-anywhere.herokuapp.com/';
+    const recipePuppyUrl = 'http://www.recipepuppy.com/api/';
+
+    fetch(conversionUrl + recipePuppyUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(result) {
+      app.recipes = result;
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  background-color: #eee;
+  padding: 50px 0 50px 0;
+  margin: 5px;
+}
+.card a {
+  color: #c20000;
+}
+</style>
